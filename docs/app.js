29fc687c852b26fe3d233530230bb1d2dd4cbb6c
Natalia <natalia.contactod@gmail.com>
2023-05-29 13:47:01 -0500

    Entrega final

--- docs/app.js
+++ docs/app.js
@@ -10,3 +10,31 @@ toggleBtn.onclick = function () {
   ? 'fa-solid fa-xmark'
   : 'fa-solid fa-bars'
 }
+
+
+// FUNCIONALIDAD FORMULARIO 
+
+const modal = document.getElementById("modal");
+const cerrarBtn = document.getElementsByClassName("closed")[0];
+
+document.getElementById("myForm").addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  // Para obtener los valores del formulario
+  const nombre = document.getElementById("nombre").value;
+  const email = document.getElementById("email").value;
+  const mensaje = document.getElementById("mensaje").value;
+  const datosModal = document.getElementById("datos-modal");
+  datosModal.textContent = nombre;
+  modal.style.display = "block";
+
+  console.log("Nombre:", nombre);
+  console.log(email);
+  console.log(mensaje);
+});
+
+cerrarBtn.addEventListener("click", function () {
+  modal.style.display = "none";
+  location.reload();
+  
+});
